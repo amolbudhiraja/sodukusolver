0732ab73ca1891bf0247baed18c8fd58b561e821
Amol Budhiraja <abudhiraja@berkeley.edu>
2023-01-30 20:23:56 -0800

    Code Revisions and Syntax Updates

--- sodukusolver/displaySodukuBoard.cpp
+++ sodukusolver/displaySodukuBoard.cpp
@@ -15,12 +15,12 @@ using namespace cv;
 /** Display an image representation of the Soduku Board. */
 void displaySodokuBoard(vector<vector<char>> &board) {
     Mat img(450, 450, CV_8UC3, Scalar(255, 255, 255));
-    int x_offset = 0, y_offset = 0;
+    int xOffset = 0, yOffset = 0;
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            rectangle(img, Point(x_offset + j * 50, y_offset + i * 50), Point(x_offset + (j + 1) * 50, y_offset + (i + 1) * 50), Scalar(0, 0, 0), 1);
+            rectangle(img, Point(xOffset + j * 50, yOffset + i * 50), Point(xOffset + (j + 1) * 50, yOffset + (i + 1) * 50), Scalar(0, 0, 0), 1);
             if (board[i][j] != '.') {
-                putText(img, string(1, board[i][j]), Point(x_offset + 25 + j * 50, y_offset + 25 + i * 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
+                putText(img, string(1, board[i][j]), Point(xOffset + 25 + j * 50, yOffset + 25 + i * 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 0), 2);
             }
         }
     }
--- sodukusolver/sodukuSolver.cpp
+++ sodukusolver/sodukuSolver.cpp
@@ -13,12 +13,15 @@ using namespace std;
 
 /** Checks to see if a given number placement is a valid placement. */
 bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+    // Check the row
     for (int x = 0; x < 9; x++)
         if (board[row][x] == num)
             return false;
+    // Check the column
     for (int y = 0; y < 9; y++)
         if (board[y][col] == num)
             return false;
+    // Check the surrounding 3x3 subgrid.
     int subgridRow = row - row % 3;
     int subgridCol = col - col % 3;
     for (int x = 0; x < 3; x++)
@@ -29,8 +32,8 @@ bool isValid(vector<vector<char>>& board, int row, int col, char num) {
 }
 
 /** Make sure that it is safe to place the number in the position on the board. */
-bool isSafe(vector<vector<char>>& board, int row, int col, char num) {
-    return board[row][col] == '.' && isValid(board, row, col, num);
+bool isSafeToPlace(vector<vector<char>>& board, int row, int col, char num) {
+    return isValid(board, row, col, num) && board[row][col] == '.';
 }
 
 /** Solve the Soduku Board. */
@@ -39,7 +42,7 @@ bool solveSudoku(vector<vector<char>>& board) {
         for (int col = 0; col < 9; col++) {
             if (board[row][col] == '.') {
                 for (char num = '1'; num <= '9'; num++) {
-                    if (isSafe(board, row, col, num)) {
+                    if (isSafeToPlace(board, row, col, num)) {
                         board[row][col] = num;
                         if (solveSudoku(board))
                             return true;
